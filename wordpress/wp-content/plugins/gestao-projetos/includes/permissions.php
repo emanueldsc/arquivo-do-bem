eeb7306edaba5df15521b2237b6885f71507057a
Emanuel Douglas <emanuel.douglas.sc@gmail.com>
2025-09-25 04:04:30 -0300

    Criando custom post types

--- wordpress/wp-content/plugins/gestao-projetos/includes/permissions.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Força posts de alunos como "pendente" até revisão do professor
-function gp_force_pending($post_data) {
-    if (current_user_can('aluno') && $post_data['post_type'] === 'projeto') {
-        $post_data['post_status'] = 'pending';
-    }
-    return $post_data;
-}
-add_filter('wp_insert_post_data', 'gp_force_pending');
